4a3cc7bf1ab8e3ac200c31daf034aa83b5427e7c
guillaume-genois <guillaumegenois@yahoo.ca>
2024-09-16 23:40:31 -0400

    permet utilisation ubuntu

--- Scripts/os_path.py
+++ Scripts/os_path.py
@@ -6,6 +6,8 @@ if platform.system() == "Windows":
     EXTRACTED_PATH = "D:/Projet Curation des métadonnées"
     DOWNLOAD_PATH = "C:/Users/guill/Downloads"
     FIREFOX_PROFILE_PATH = "C:/Users/guill/AppData/Roaming/Mozilla/Firefox/Profiles/4am1ne92.default-release-1609958750563"
+# C:\\Users\\guill\\.cache\\selenium\\geckodriver\\win64\\0.35.0\\geckodriver.exe
+
 elif platform.system() == "Linux":
     MAIN_PATH = "/home/guillaume-genois/PycharmProjects/IFT3150-ProjetCurationMetadonnees"
     EXTRACTED_PATH = "/media/guillaume-genois/Samsung USB/Projet Curation des métadonnées"
