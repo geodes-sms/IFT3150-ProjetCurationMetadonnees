b9028960ea967eb7c3674e3d883d9e0295498451
ggenois <guillaumegenois@yahoo.ca>
2025-03-10 08:16:15 -0400

    ajout path demo en commentaire

--- Scripts/os_path.py
+++ Scripts/os_path.py
@@ -10,8 +10,8 @@ if platform.system() == "Windows":
 
 elif platform.system() == "Linux":
     MAIN_PATH = "/home/ggenois/PycharmProjects/IFT3150-ProjetCurationMetadonnees"
-    EXTRACTED_PATH = "/media/ggenois/Samsung USB/Demo"
-    # EXTRACTED_PATH = "/media/ggenois/Samsung USB/Projet Curation des métadonnées"
+    # EXTRACTED_PATH = "/media/ggenois/Samsung USB/Demo"
+    EXTRACTED_PATH = "/media/ggenois/Samsung USB/Projet Curation des métadonnées"
     DOWNLOAD_PATH = "/home/ggenois/Downloads"
     FIREFOX_PROFILE_PATH = "/home/ggenois/snap/firefox/common/.mozilla/firefox/er60u9dx.default"
 else:
